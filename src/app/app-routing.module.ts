0e9ff4f6c193632f6f74defe63373ea66b067e8c
matheeshaYapa <ylmatheesha@gmail.com>
2019-11-01 15:51:15 +0530

    Added initial dependencies and codes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BananaComponent} from './banana/banana.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BananaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
